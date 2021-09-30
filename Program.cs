940f62cbab8bf85a30f1290c8d589eabbe8a7d8d
Sanier <Sanier20@mail.ru>
2021-09-30 19:45:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Obj_7._2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        class A
+        {
+            public virtual void Display()
+            {
+                Console.WriteLine("A");
+            }
+        }
+
+        class B : A
+        {
+            public new void Display()
+            {
+                //base.Display();
+                Console.WriteLine("B");
+            }
+        }
+
+        class C : A
+        {
+            public override void Display()
+            {
+                //base.Display();
+                Console.WriteLine("C");
+
+            }
+        }
+
+        class D : B
+        {
+            public new void Display()
+            {
+                Console.WriteLine("D");
+            }
+        }
+
+        class E : C
+        {
+            public new void Display()
+            {
+                Console.WriteLine("E");
+            }
+        }
+    }
+}
